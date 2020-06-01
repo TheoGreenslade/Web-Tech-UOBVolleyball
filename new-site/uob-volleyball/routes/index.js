b20fad591e26c337924a682b19eca49abd99d1d7
TheoGreenslade <theogreenslade@outlook.com>
2020-06-01 15:14:16 +0100

    added training, about, committee and contact pages

--- new-site/uob-volleyball/routes/index.js
+++ new-site/uob-volleyball/routes/index.js
@@ -10,4 +10,20 @@ router.get('/shop', function(req, res, next) {
   res.render('shop', { title: 'Shop' });
 });
 
+router.get('/training', function(req, res, next) {
+  res.render('training', { title: 'Training' });
+});
+
+router.get('/about-the-club', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+router.get('/committee', function(req, res, next) {
+  res.render('committee', { title: 'Committee' });
+});
+
+router.get('/contact-us', function(req, res, next) {
+  res.render('contact', { title: 'Contact us' });
+});
+
 module.exports = router;
