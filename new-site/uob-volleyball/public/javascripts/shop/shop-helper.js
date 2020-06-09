5e58505c7f99f5384fda126d41233f95a48a4739
DavideBernardi <53653086+DavideBernardi@users.noreply.github.com>
2020-06-09 04:01:20 +0100

    Moving JS to external script doesn't work :(

---
+++ new-site/uob-volleyball/public/javascripts/shop/shop-helper.js
@@ -0,0 +1,18 @@
+function displayTicked() {
+    var categories = {{{json categories}}};
+    var products = {{{json products}}};
+    for (var i = 0; i<categories.length; i++) {
+       for(var j = 0; j<products.length; j++) {
+            var checkBox = document.getElementById("inlineCheckbox"+categories[i].id);
+            var box = document.getElementById("productBox"+products[j].product_id);
+            if(products[j].category_name == categories[i].name) {
+                if (checkBox.checked == true){
+                    box.style.display = "block";
+                } else {
+                    box.style.display = "none";
+                }
+            }
+        }
+    }
+}
+
