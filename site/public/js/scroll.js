cfec9f81cc650139b9caa1cf32bce86b4f134f13
TheoGreenslade <theogreenslade@outlook.com>
2020-07-13 14:39:02 +0100

    rearrange folders

--- site/public/js/scroll.js
+++
@@ -1,4 +0,0 @@
-function scrollDown(){
-    var elmnt = document.getElementById("intro");
-    elmnt.scrollIntoView({behavior: "smooth", block: "start"});
-}
--- site/public/js/vueshit.js
+++
@@ -1,5 +0,0 @@
-// "use strict";
-var app = new Vue({
-    el: '#app',
-    data: { message: 'I love volleyball!' }
-  })
